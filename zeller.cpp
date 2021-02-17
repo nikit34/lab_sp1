af677c621497e1b700e0b2b5a6281fae9d933c64
nikit34 <permikov134@mail.ru>
2021-02-17 21:49:56 +0300

    igly

--- zeller.cpp
+++ zeller.cpp
@@ -9,22 +9,22 @@ extern double mm;
 extern double yyyy;
 double day__of__week(){
  return zeller(dd,
-  (mm < 3 ? mm + 10. : mm - 2.),
+  (mm < 3. ? mm + 10. : mm - 2.),
   remainder((mm < 3. ? yyyy - 1. : yyyy), 100.),
   quotient((mm < 3. ? yyyy - 1. : yyyy), 100.)
  );
 }
 double explicit_form(double d, double m, double y, double c){
-    ((d + y) > 0) ? ((quotient(26 * m - 2, 10)) ? quotient(y, 4) : quotient(c, 4)) : ((2 * -1 * c));
+    ((d + y) > 0.) ? ((quotient(26. * m - 2., 10.)) ? quotient(y, 4.) : quotient(c, 4.)) : ((2. * -1. * c));
     return c;
 }
 double zeller(double d, double m, double y, double c){
  return neg__to__pos(remainder(
-        explicit_form(d, m, y, c), 7
+        explicit_form(d, m, y, c), 7.
  ));
 }
 //(define (neg-to-pos d)
-// (cond((< d 0)(+ d 7))
+// (cond((< d 0.)(+ d 7.))
 //      (#t d)
 // )
 //)
